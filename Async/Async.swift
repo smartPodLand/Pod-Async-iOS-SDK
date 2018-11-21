581b09a5cdf1938bed7013008eaf8f7cca165e58
Mahyar <mahyarjananeh289@gmail.com>
2018-11-21 18:30:51 +0330

    create a model for swiftyBeaver+ fix some bugs

--- Async/Async.swift
+++ Async/Async.swift
@@ -11,8 +11,7 @@ import Starscream
 import SwiftyJSON
 import SwiftyBeaver
 
-// use this property to get putput logs
-let log = SwiftyBeaver.self
+public let log = LogWithSwiftyBeaver().log
 
 // this is the Async class that will handles Asynchronous messaging
 public class Async {
@@ -70,8 +69,6 @@ public class Async {
             self.reconnectOnClose = true
         }
         
-        // firt step is to config output logs
-        configLogs()
     }
     
     
@@ -113,82 +110,6 @@ public class Async {
     var registerServerTimeoutIdTimer: RepeatingTimer?
     
     var socket: WebSocket?
-    
-    
-    // MARK: - config Logs
-    func configLogs() {
-        // add log destinations. at least one is needed!
-        
-        // 1-------------------------------------------------------------
-        // MARK: - log to Xcode Console:
-        let console = ConsoleDestination()
-        
-        // setup format of loging to Xcode Console
-        let consoleFormatString: String = """
-
-________________________________________________________________________________________________
-||--------------------------------------------------------------------------------------------||
-|| $C$L                                                                                  ||
-||  _____________________________________________________________________                     ||
-|| |     time     | line | class.function\t\t\t\t\t| thread
-|| | $U |  $l  | $N.$F\t\t| $T
-||  ---------------------------------------------------------------------                     ||
-|| | Message:                                                                                 ||
-|| | $M
-||  ---------------------------------------------------------------------                     ||
-|| | Additional information:                                                                  ||
-|| | $X
-||  ---------------------------------------------------------------------                     ||
-||____________________________________________________________________________________________||
-------------------------------------------------------------------------------------------------
-
-"""
-        console.format = consoleFormatString
-        
-        /*
-         // set global minLevel (verbose, info, debug, warning, error)
-         console.minLevel = .error
-         */
-        
-        /*
-         // set Path Filter to a certain class
-         let filter1 = Filters.Path.contains("UnicornViewController",minLevel: .debug)
-         console.addFilter(filter1)
-         */
-        
-        /*
-         // set Function Filter to a certain function of a certain class
-         let filter1 = Filters.Path.contains("UnicornViewController", required: true)
-         let filter2 = Filters.Function.contains("viewDidLoad", required: true)
-         console.addFilter(filter1)
-         console.addFilter(filter2)
-         */
-        
-        /*
-         // Silence File with Exclusion:
-         console.addFilter(Filters.Path.excludes("Horse", required: true)
-         */
-        
-        log.addDestination(console)
-        
-        
-        // 2-------------------------------------------------------------
-        // MARK: - log to default swiftybeaver.log file
-        let file = FileDestination()
-        /*
-         // Log Certain Message to an own Destination
-         file.logFileURL = URL(string: "file:///tmp/myNetworkLogs.log")
-         file.addFilter(Filters.Message.contains("HTTP", caseSensitive: true, required: true))
-         */
-        log.addDestination(file)
-        
-        
-        // 3-------------------------------------------------------------
-        // MARK: -
-        let platform = SBPlatformDestination(appID: "Z5RkJe", appSecret: "UDxfbvnmxyin4kydENQprhw5tths0bvo", encryptionKey: "t9ugl6wkcvp9w2yRvma0ykpfqy94TjzI")
-        
-        log.addDestination(platform)
-    }
     
     // MARK: - Create Socket Connection
     /*
